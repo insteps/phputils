1c7d45c97e76c4524155116f8f9376f5315bf3df
V.Krishn <vkrishn4@gmail.com>
2013-03-06 06:23:05 +0530

    add str_getcsv4

---
+++ str_getcsv4.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) 2012-2013 V.Krishn <vkrishn4@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  PHP
+ * @author    V.Krishn <vkrishn4@gmail.com>
+ * @copyright V.Krishn <vkrishn4@gmail.com>, All rights reserved.
+ * @license   GPL
+ * @link      http://github.com/insteps/phputils
+ * @name      str_getcsv4
+ * @version   0.1.0
+ *
+ */
+
+/**
+  * Parse a CSV string into an array for php 4+.
+  * @param string $input String
+  * @param string $delimiter String
+  * @param string $enclosure String
+  * @return array
+  */
+function str_getcsv4($input, $delimiter = ',', $enclosure = '"') {
+
+    if( ! preg_match("/[$enclosure]/", $input) ) {
+      return (array)preg_replace(array("/^\\s*/", "/\\s*$/"), '', explode($delimiter, $input));
+    }
+
+    $token = "##"; $token2 = "::";
+    $t1 = preg_replace(array("/\\\[$enclosure]/", "/$enclosure{2}/",
+         "/[$enclosure]\\s*[$delimiter]\\s*[$enclosure]\\s*/", "/\\s*[$enclosure]\\s*/"),
+         array($token2, $token2, $token, $token), trim(trim(trim($input), $enclosure)));
+
+    $a = explode($token, $t1);
+    foreach($a as $k=>$v) {
+        if ( preg_match("/^{$delimiter}/", $v) || preg_match("/{$delimiter}$/", $v) ) {
+            $a[$k] = trim($v, $delimiter); $a[$k] = preg_replace("/$delimiter/", "$token", $a[$k]); }
+    }
+    $a = explode($token, implode($token, $a));
+    return (array)preg_replace(array("/^\\s/", "/\\s$/", "/$token2/"), array('', '', $enclosure), $a);
+
+}
+
+
